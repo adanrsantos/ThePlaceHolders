c0b826afa81201d336370b1e4a971b2dc5e2a3bb
Logan <logan@gatlintc.com>
2024-09-27 14:37:49 -0500

    Fixed bug with session handling

--- go.mod
+++ go.mod
@@ -4,4 +4,7 @@ go 1.23
 
 require github.com/gorilla/sessions v1.4.0
 
-require github.com/gorilla/securecookie v1.1.2 // indirect
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/gorilla/securecookie v1.1.2 // indirect
+)
--- users.go
+++ users.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -15,77 +18,108 @@ type UserData struct {
 	LastLogin      time.Time `json:"last-login"`
 }
 
+// Handles requests to /login.html
 func (s *Server) handle_login(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	switch r.Method {
+	case http.MethodGet:
+		http.ServeFile(w, r, "./static/register.html")
+	case http.MethodPost:
+		// Get data from form
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		// Get user from database
+		user, ok := s.Users[email]
+		// If user does not exist
+		if !ok {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		// If password does not match
+		if password != user.Password {
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		// Generate session
+		session, err := s.Sessions.Get(r, SESSION_COOKIE_NAME)
+		if err != nil {
+			s.handle_logout(w, r)
+			http.Error(w, "Invalid session", http.StatusUnauthorized)
+			return
+		}
+		session.Values[SESSION_AUTH] = true
+		session.Save(r, w)
+		// Update last-login on DB
+		user.LastLogin = time.Now()
+		s.Users[email] = user
+		// Redirect to index.html
+		http.Redirect(w, r, "/", http.StatusFound)
+	default:
 		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
 	}
-	// Get data from form
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-	// Get user from database
-	user, ok := s.Users[email]
-	// If user does not exist
-	if !ok {
-		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
-	}
-	// If password does not match
-	if password != user.Password {
-		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
-	}
-	// Generate session
-	session, err := s.Sessions.Get(r, SESSION_COOKIE_NAME)
-	if err != nil {
-		http.Error(w, "Internal Error", http.StatusInternalServerError)
-		return
-	}
-	session.Values[SESSION_AUTH] = true
-	if err := session.Save(r, w); err != nil {
-		http.Error(w, "Internal Error", http.StatusInternalServerError)
-		return
-	}
-	// Update last-login on DB
-	user.LastLogin = time.Now()
-	s.Users[email] = user
-	// Redirect to index.html
-	http.Redirect(w, r, "/", http.StatusFound)
 }
 
+// Handles requests to /register.html
 func (s *Server) handle_register(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+	switch r.Method {
+	case http.MethodGet:
+		http.ServeFile(w, r, "./static/register.html")
+	case http.MethodPost:
+		// Get data from form
+		email := r.FormValue("email")
+		password := r.FormValue("password")
+		fmt.Println(r.Form)
+		// Check that this email is not already registered
+		if _, ok := s.Users[email]; ok {
+			fmt.Println("Already registered")
+			http.Error(w, "Forbidden", http.StatusForbidden)
+			return
+		}
+		// Generate session
+		session, err := s.Sessions.Get(r, SESSION_COOKIE_NAME)
+		// If session cookie invalid
+		if err != nil {
+			s.handle_logout(w, r)
+			http.Error(w, "Invalid session", http.StatusUnauthorized)
+			return
+		}
+		// Save user information to DB
+		s.Users[email] = UserData{
+			Email:          email,
+			Password:       password,
+			AccountCreated: time.Now(),
+			LastLogin:      time.Now(),
+		}
+		// Make session valid
+		session.Values[SESSION_AUTH] = true
+		// Send session token to browser
+		session.Save(r, w)
+		// Redirect to index.html
+		s.save_state()
+		http.Redirect(w, r, "/", http.StatusFound)
+	default:
 		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
 	}
-	// Get data from form
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-	// Check that this email is not already registered
-	if _, ok := s.Users[email]; ok {
-		http.Error(w, "Forbidden", http.StatusForbidden)
-		return
+}
+
+func (s *Server) handle_logout(w http.ResponseWriter, r *http.Request) {
+	// If session exists
+	if session, err := s.Sessions.Get(r, SESSION_COOKIE_NAME); err == nil {
+		// Remove authorization
+		session.Values[SESSION_AUTH] = false
+		session.Save(r, w)
 	}
-	// Generate session
-	session, err := s.Sessions.Get(r, SESSION_COOKIE_NAME)
+	// Remove session cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:   SESSION_COOKIE_NAME,
+		MaxAge: -1,
+	})
+}
+
+func (s *Server) save_state() {
+	file, err := os.Create("db.json")
 	if err != nil {
-		http.Error(w, "Internal Error", http.StatusInternalServerError)
-		return
+		panic(err)
 	}
-	// Save user information to DB
-	s.Users[email] = UserData{
-		Email:          email,
-		Password:       password,
-		AccountCreated: time.Now(),
-		LastLogin:      time.Now(),
-	}
-	// Make session valid
-	session.Values[SESSION_AUTH] = true
-	// Send session token to browser
-	if err := session.Save(r, w); err != nil {
-		http.Error(w, "Internal Error", http.StatusInternalServerError)
-		return
-	}
-	// Redirect to index.html
-	http.Redirect(w, r, "/", http.StatusFound)
+	defer file.Close()
+	json.NewEncoder(file).Encode(s.Users)
 }
